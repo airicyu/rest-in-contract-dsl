f28808557103c99aaf0090cb16801dbc6d7efaf8
airicyu <airic.yu@gmail.com>
2017-05-12 21:54:00 +0800

    freezing function objects to prevent runtime altering

--- index.js
+++ index.js
@@ -2,4 +2,19 @@
 
 const contractDSL = require('./src/dsl');
 
+/* freeze the dsl function objects to prevent runtime altering */
+function deepFreeze(obj) {
+  var propNames = Object.getOwnPropertyNames(obj);
+
+  propNames.forEach(function(name) {
+    var prop = obj[name];
+    if (typeof prop == 'object' && prop !== null)
+      deepFreeze(prop);
+  });
+
+  return Object.freeze(obj);
+}
+
+deepFreeze(contractDSL);
+
 module.exports = contractDSL;
